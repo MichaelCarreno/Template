424e62fcb4f168553f9168ede0bc3c70dc1132ae
MichaelCarreno <166785079+MichaelCarreno@users.noreply.github.com>
2024-05-28 12:55:32 -0500

    Template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { PreciosComponent } from './components/precios/precios.component';
 import { ProtegidoComponent } from './components/protegido/protegido.component';
+import { TemplateFormsComponent } from './template-forms/template-forms.component';
 
 const routes: Routes = [
   {path: 'home', component:HomeComponent},
   {path: 'precios', component:PreciosComponent},
   {path: 'protegido', component:ProtegidoComponent},
+  {path: 'template', component:TemplateFormsComponent},
   {path: '**', pathMatch: "full", redirectTo:'home'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { HomeComponent } from './components/home/home.component';
 import { PreciosComponent } from './components/precios/precios.component';
 import { ProtegidoComponent } from './components/protegido/protegido.component';
 import { AuthModule } from '@auth0/auth0-angular';
+import { TemplateFormsComponent } from './template-forms/template-forms.component';
 
 @NgModule({
   declarations: [
@@ -15,11 +17,13 @@ import { AuthModule } from '@auth0/auth0-angular';
     NavbarComponent,
     HomeComponent,
     PreciosComponent,
-    ProtegidoComponent
+    ProtegidoComponent,
+    TemplateFormsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     AuthModule.forRoot({
       domain: 'dev-ghfpm3oinkvvvutq.us.auth0.com',
       clientId: 'jeLx7BPrw0be2WBGa3X1AxP7irvNN8s0',
